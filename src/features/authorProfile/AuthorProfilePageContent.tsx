854312086fd2a791a7ac82d19f95272079c5b337
sriipu1004-wq <sriipu1004@gmail.com>
2026-06-26 16:42:06 +0900

    Remove unused author profile module

--- src/features/authorProfile/AuthorProfilePageContent.tsx
+++
@@ -1,5 +0,0 @@
-import Link from "next/link";
-
-export default function AuthorProfilePageContent({ authorId }: { authorId: string }) {
-  return <main><Link href={`/authors/${authorId}`}>作者ページ</Link></main>;
-}
